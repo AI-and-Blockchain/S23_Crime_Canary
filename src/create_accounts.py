a4defef90b61f18d595b6b9a358920c88db191b7
ChibuikemEzemaduka <91480660+ChibuikemEzemaduka@users.noreply.github.com>
2023-04-13 14:21:49 -0400

    Update create_accounts.py

--- src/create_accounts.py
+++ src/create_accounts.py
@@ -6,11 +6,9 @@ f_object = 'accounts.csv'
 
 if not(os.path.exists(f_object)):
     policeWallet = account.generate_account()
-    OracleCreator = account.generate_account()
     participant = account.generate_account()
     
     pol_secret = mnemonic.from_private_key(policeWallet[0])
-    orac_secret = mnemonic.from_private_key(OracleCreator[0])
     par_secret = mnemonic.from_private_key(participant[0])
     
     with open(f_object, 'w', newline='') as f_object:
@@ -18,8 +16,7 @@ if not(os.path.exists(f_object)):
         
         writer.writerow(["ID", "TYPE", "PUBLIC", "PRIVATE", "SECRET"])
         writer.writerow([1, "POLICE", policeWallet[1], policeWallet[0], pol_secret])
-        writer.writerow([2, "ORACLE", OracleCreator[1], OracleCreator[0], orac_secret])
-        writer.writerow([3, "USER", participant[1], participant[0], par_secret])
+        writer.writerow([2, "USER", participant[1], participant[0], par_secret])
 
 else:    
     no_lines = len(pd.read_csv(f_object))
