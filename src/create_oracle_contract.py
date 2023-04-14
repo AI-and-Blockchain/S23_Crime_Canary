9a761928895ebe1ea109482efa6ed746615a6832
ChibuikemEzemaduka <91480660+ChibuikemEzemaduka@users.noreply.github.com>
2023-04-14 02:33:56 -0400

    Update create_oracle_contract.py

--- src/create_oracle_contract.py
+++ src/create_oracle_contract.py
@@ -3,7 +3,7 @@ from oracle_utils import create_deploy_oracle
 import csv, os
 
 
-c_object = 'oracle_owner.csv'
+c_object = '../oracle_owner.csv'
 
 #Retrieve Oracle owner details
 with open(c_object, 'r') as csv_file:
@@ -12,7 +12,7 @@ with open(c_object, 'r') as csv_file:
     private_key = csv_reader[1][3]
 
 
-f_object = 'oracle_contract.csv'
+f_object = '../oracle_contract.csv'
 if not(os.path.exists(f_object)):
     app_id = create_deploy_oracle(public_key, private_key)
     print("Created application ID is: ", app_id)
